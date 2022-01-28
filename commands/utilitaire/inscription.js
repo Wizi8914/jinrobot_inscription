9347f40a12278e841b621c7380243271ac7c1896
Wizi8914 <loick.konieczny@gmail.com>
2022-01-28 14:20:04 +0100

    updade

--- commands/utilitaire/inscription.js
+++ commands/utilitaire/inscription.js
@@ -265,8 +265,6 @@ module.exports = class SayCommand extends Command {
                                             }
 
                                         })
-
-
                                     }
                                 })
 
